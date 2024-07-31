d9723f53753b83562d11ad12e782de0e34bd27a2
OdranPrigent <odran.prigent@hotmail.fr>
2024-07-31 12:59:24 +0200

    creation des routes

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,14 @@
 const express = require('express');
 
 const app = express();
+const stuffRoutes = require('./routes/stuff');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://Odran:test@cluster0.dtmqq99.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    { useNewUrlParser: true,
+      useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -9,26 +17,6 @@ app.use((req, res, next) => {
     next();
   });
 
-app.use('/api/books', (req, res, next) => {
-    const stuff = [
-      {
-        _id: 'oeihfzeoi',
-        title: 'Mon premier objet',
-        description: 'Les infos de mon premier objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 4900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'oeihfzeomoihi',
-        title: 'Mon deuxième objet',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-    ];
-    res.status(200).json(stuff);
-  });
+  app.use('/api/stuff', stuffRoutes)
 
 module.exports = app;
---
+++ backend/routes/stuff.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const Thing = require('../models/Thing')
+const router = express.Router();
+
+router.post('/auth/login', (req, res, next) => {
+    delete req.body._id;
+    const thing = new Thing({
+        ...req.body
+    });
+    thing.save()
+        .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+        .catch(error => res.status(400).json({ error }));
+});
+
+router.use('/books', (req, res, next) => {
+    Thing.find()
+    .then(things => res.status(200).json(things))
+    .catch(error => res.status(400).json({ error }));
+  });
+
+module.exports = router;
