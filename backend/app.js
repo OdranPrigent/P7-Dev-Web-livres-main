d63c70dee00cbf4308a39c452b84ce842be3d777
OdranPrigent <odran.prigent@hotmail.fr>
2024-07-31 15:12:53 +0200

    preparation connectionUser

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const app = express();
 const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb+srv://Odran:test@cluster0.dtmqq99.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
@@ -17,6 +18,7 @@ app.use((req, res, next) => {
     next();
   });
 
-  app.use('/api/stuff', stuffRoutes)
+  app.use('/api/stuff', stuffRoutes);
+  app.use('/api/auth', userRoutes);
 
 module.exports = app;
--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -1,21 +1,9 @@
 const express = require('express');
 const Thing = require('../models/Thing')
+const stuff = require('../controllers/stuff')
 const router = express.Router();
 
-router.post('/auth/login', (req, res, next) => {
-    delete req.body._id;
-    const thing = new Thing({
-        ...req.body
-    });
-    thing.save()
-        .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-        .catch(error => res.status(400).json({ error }));
-});
-
-router.use('/books', (req, res, next) => {
-    Thing.find()
-    .then(things => res.status(200).json(things))
-    .catch(error => res.status(400).json({ error }));
-  });
+router.post('/auth/login', stuff.login);
+router.use('/books', stuff.books );
 
 module.exports = router;
